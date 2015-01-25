b9bdfaac5b4a9ed135c5077c8c1de1ae433512d7
Ralph Marschall <ralph_marschall@hotmail.com>
2015-01-25 21:22:53 +0100

    fix launch script

--- OmpSS/launch_hpc_benchmark.py
+++ OmpSS/launch_hpc_benchmark.py
@@ -27,10 +27,8 @@ print '--- Running '+str(number_of_nodes)+' times '+path_c_file_to_run+' ---'
 for i in range(start, number_of_nodes + 1):
     #command_1 = 'export OMP_NUM_THREADS='+str(i)
     os.environ["OMP_NUM_THREADS"] = str(i)
-    command_2 = './' + path_c_file_to_run
-    print 'Running 1/2: '+ command_1
-    os.system(command_1)
-    print 'Running 2/2: '+ command_2
-    os.system(command_2)
+    command = './' + path_c_file_to_run
+    print 'Running: '+ command
+    os.system(command)
 
 print '--- End Running ---'
